7586ddf1c4f01d1c8b93d194e26085528742b303
天天困 <2570024918@qq.com>
2025-08-15 20:04:41 +0800

    RAG知识库代码初始化

--- src/main/java/com/kun/aikunhelper/ai/AiCodeHelperService.java
+++ src/main/java/com/kun/aikunhelper/ai/AiCodeHelperService.java
@@ -3,8 +3,15 @@ package com.kun.aikunhelper.ai;
 
 import dev.langchain4j.service.SystemMessage;
 
+import java.util.List;
+
 public interface AiCodeHelperService {
 
     @SystemMessage(fromResource = "system-prompt.txt")
     String chat(String userMessage);
+
+    @SystemMessage(fromResource = "system-prompt.txt")
+    Report chatForReport(String userMessage);
+
+    record Report(String name, List<String> suggestionList){}
 }
--- src/test/java/com/kun/aikunhelper/ai/AiCodeHelperServiceTest.java
+++ src/test/java/com/kun/aikunhelper/ai/AiCodeHelperServiceTest.java
@@ -25,4 +25,11 @@ class AiCodeHelperServiceTest {
         result = aiCodeHelperService.chat("你好，你知道我是谁吗");
         System.out.println(result);
     }
+
+    @Test
+    void chatForReport() {
+        String userMessage ="你好我是天天困，我学编程两年了，请你帮我制作一份面试题";
+        AiCodeHelperService.Report report = aiCodeHelperService.chatForReport(userMessage);
+        System.out.println(report);
+    }
 }
